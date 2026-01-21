44c829b0b98460ae0a457215f4bf37105a8af48b
Clara Garcia-Sanchez <45241020+gsclara@users.noreply.github.com>
2026-01-21 15:52:08 +0100

    new BC with displacement height and local terrain height ESI

---
+++ of7/src/atmosphericModels/derivedFvPatchFields/atmdBoundaryLayerInletK/atmdBoundaryLayerInletKFvPatchScalarField.C
@@ -0,0 +1,153 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "atmdBoundaryLayerInletKFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+atmdBoundaryLayerInletKFvPatchScalarField::
+atmdBoundaryLayerInletKFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletFvPatchScalarField(p, iF),
+    atmdBoundaryLayer(p.patch())
+{}
+
+
+atmdBoundaryLayerInletKFvPatchScalarField::
+atmdBoundaryLayerInletKFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    inletOutletFvPatchScalarField(p, iF),
+    atmdBoundaryLayer(patch().Cf(), dict, p.patch())
+{
+    phiName_ = dict.lookupOrDefault<word>("phi", "phi");
+
+    refValue() = k(patch().Cf());
+    refGrad() = 0;
+    valueFraction() = 1;
+
+    if (dict.found("value"))
+    {
+        scalarField::operator=(scalarField("value", dict, p.size()));
+    }
+    else
+    {
+        scalarField::operator=(refValue());
+    }
+}
+
+
+atmdBoundaryLayerInletKFvPatchScalarField::
+atmdBoundaryLayerInletKFvPatchScalarField
+(
+    const atmdBoundaryLayerInletKFvPatchScalarField& psf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    inletOutletFvPatchScalarField(psf, p, iF, mapper),
+    atmdBoundaryLayer(psf, p, mapper)
+{}
+
+
+atmdBoundaryLayerInletKFvPatchScalarField::
+atmdBoundaryLayerInletKFvPatchScalarField
+(
+    const atmdBoundaryLayerInletKFvPatchScalarField& psf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    inletOutletFvPatchScalarField(psf, iF),
+    atmdBoundaryLayer(psf)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void atmdBoundaryLayerInletKFvPatchScalarField::autoMap
+(
+    const fvPatchFieldMapper& m
+)
+{
+    inletOutletFvPatchScalarField::autoMap(m);
+    atmdBoundaryLayer::autoMap(m);
+}
+
+
+void atmdBoundaryLayerInletKFvPatchScalarField::rmap
+(
+    const fvPatchScalarField& psf,
+    const labelList& addr
+)
+{
+    inletOutletFvPatchScalarField::rmap(psf, addr);
+
+    const atmdBoundaryLayerInletKFvPatchScalarField& blpsf =
+        refCast<const atmdBoundaryLayerInletKFvPatchScalarField>(psf);
+
+    atmdBoundaryLayer::rmap(blpsf, addr);
+}
+
+
+void atmdBoundaryLayerInletKFvPatchScalarField::write(Ostream& os) const
+{
+    fvPatchScalarField::write(os);
+    atmdBoundaryLayer::write(os);
+    writeEntryIfDifferent<word>(os, "phi", "phi", phiName_);
+    writeEntry(os, "value", *this);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField
+(
+    fvPatchScalarField,
+    atmdBoundaryLayerInletKFvPatchScalarField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
